aabe0353a8d153ed7d8f034434a6098520ce0094
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-08-10 22:39:50 +0200

    Layout

---
+++ src/main/java/com/media_collection/frontend/views/MainLayout.java
@@ -0,0 +1,37 @@
+package com.media_collection.frontend.views;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
+public class MainLayout extends AppLayout {
+    public MainLayout() {
+        createHeader();
+        createDrawer();
+    }
+    private void createHeader() {
+        H1 header1 = new H1("Media collection frontend");
+        header1.addClassNames(
+                LumoUtility.FontSize.LARGE,
+                LumoUtility.Margin.MEDIUM);
+        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), header1);
+        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        header.expand(header1);
+        header.setWidthFull();
+        header.addClassNames(
+                LumoUtility.Padding.Vertical.NONE,
+                LumoUtility.Padding.Horizontal.MEDIUM);
+        addToNavbar(header);
+    }
+
+    private void createDrawer() {
+        addToDrawer(new VerticalLayout(
+                new RouterLink("Welcome page", WelcomePage.class)
+        ));
+    }
+}
---
+++ src/main/java/com/media_collection/frontend/views/WelcomePage.java
@@ -0,0 +1,37 @@
+package com.media_collection.frontend.views;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+import org.springframework.stereotype.Component;
+
+@Route(value = "", layout = MainLayout.class)
+@PageTitle("Welcome page")
+@Component
+public class WelcomePage extends VerticalLayout {
+    public WelcomePage() {
+        addClassName("list-view");
+        setSizeFull();
+        createPage();
+    }
+    private void createPage() {
+        VerticalLayout content = new VerticalLayout();
+        content.setDefaultHorizontalComponentAlignment(Alignment.START);
+        content.setWidthFull();
+        content.addClassNames(
+                LumoUtility.Padding.Vertical.NONE,
+                LumoUtility.Padding.Horizontal.MEDIUM);
+        H1 header1 = new H1("Welcome to media collection!");
+        header1.addClassNames(
+                LumoUtility.FontSize.LARGE,
+                LumoUtility.Margin.MEDIUM);
+        Div div = new Div();
+        Paragraph paragraph = new Paragraph("Current statistics:");
+        content.add(header1,div,paragraph);
+        add(content);
+    }
+}
